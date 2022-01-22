c526eb62b00dccda3e432f5fc1311606f828396c
DanielleIzawa <izawa.danielle@gmail.com>
2022-01-21 22:20:12 -0300

    Updates

---
+++ precache-manifest.8bbea0c2320f61e0aabdd6abaf81db28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20f4640f2852a39a4884ddd08072141",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "bab6209481cc7af24875",
+    "url": "/react-app/static/css/main.e4a34950.chunk.css"
+  },
+  {
+    "revision": "10ae0603604359519bfb",
+    "url": "/react-app/static/js/2.50c414a1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-app/static/js/2.50c414a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab6209481cc7af24875",
+    "url": "/react-app/static/js/main.634b05e8.chunk.js"
+  },
+  {
+    "revision": "a0869fd3ed4d6a5b8e7c",
+    "url": "/react-app/static/js/runtime-main.d74d1e89.js"
+  }
+]);
